70035a2f7d67841ebe036d25f1e997a53df5f64d
soti-web <soti03e250058@epu.edu.iq>
2026-01-05 03:41:58 +0300

    Add files via upload

---
+++ src/Components/AddToCart/AddToCart.js
@@ -0,0 +1,20 @@
+import styles from './AddToCart.module.css';
+import React from 'react';
+import { ReactComponent as Add } from "../../assets/image/add.svg";
+
+const AddToCart = props => {
+    const {
+        game,
+        handleHoverGame,
+        handleAddToCart
+    } = props;
+
+    return (
+          <div className={styles.addToCart} onMouseEnter={handleHoverGame} onMouseLeave={handleHoverGame} id={game.id} onClick={handleAddToCart}>
+            <h4 style={{ color: game.isHovered ? "#92f" : "#999" }}>Add to cart</h4>
+            <Add className={styles.add} style={{ fill: game.isHovered ? "#92f" : "#999" }} />
+          </div>
+    );
+  }
+  
+  export default AddToCart;
